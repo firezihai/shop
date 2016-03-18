c3567bdf349d592a1d2f2760176edea48383a374
firezihai <zihaidetiandi@sina.com>
2016-03-18 23:33:46 +0800

    取消HomeActivity继承AppFragmentActivity，底部导航采用单一activity

--- gen/android/support/v7/appcompat/R.java
+++ gen/android/support/v7/appcompat/R.java
@@ -443,7 +443,7 @@ public final class R {
 		public static final int abc_textfield_search_activated_mtrl_alpha = 0x7f02003c;
 		public static final int abc_textfield_search_default_mtrl_alpha = 0x7f02003d;
 		public static final int abc_textfield_search_material = 0x7f02003e;
-		public static final int notification_template_icon_bg = 0x7f02005b;
+		public static final int notification_template_icon_bg = 0x7f02005c;
 	}
 	public static final class id {
 		public static final int action0 = 0x7f090074;
